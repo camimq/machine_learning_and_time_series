69cd4128e506f6f30a7883cf16d06603d3b0c8df
Camila <camimq85@gmail.com>
2024-02-29 16:03:12 -0300

    adiciona conteúdo da aula de Feature Engineering IV e V.

--- scripts_py/aula_3_feature_engineering.py
+++ scripts_py/aula_3_feature_engineering.py
@@ -79,3 +79,70 @@ x_std.sqt_living.hist();
 x_std.bathrooms.hist();
 
 # %%
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+# %%
+dados.head()
+
+# %%
+# divide os dados em x e y
+x = dados[['sqft_living', 'bathrooms']].values
+y = dados['price'].values
+
+# %%
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 7)
+
+# %%
+len(x_train)
+
+# %%
+len(x_test)
+
+# %%
+x_train
+
+# %%
+# usando o MinMaxScaller
+scaler = MinMaxScaler()
+
+scaler.fit(x_train)
+
+# %%
+x_train_scaled = scaler.transform(x_train)
+x_test_scaled = scaler.transform(x_test)
+
+# %%
+x_train_scaled
+
+# %%
+model = LinearRegression()
+
+model.fit(x_train_scaled, y_train)
+
+# %%
+y_pred = model.predict(x_test_scaled)
+
+# %%
+MAE = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print('MAE: ', MAE)
+print('R2: ', r2)
+
+# %%
+model_normal = LinearRegression()
+
+model_normal.fit(x_train, y_train)
+
+# %%
+y_pred_normal = model_normal.predict(x_test)
+
+# %%
+MAE = mean_absolute_error(y_test, y_pred_normal)
+r2 = r2_score(y_test, y_pred_normal)
+
+print('MAE: ', MAE)
+print('R2: ', r2)
